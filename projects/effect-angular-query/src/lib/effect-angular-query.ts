fc35ee269e86c1f3f41c241a5d3aa7bdd0243f82
Lukas heddendorp <lu.heddendorp@gmail.com>
2026-01-28 14:11:18 +0100

    Add code for tanstack query effect rpc integration

---
+++ projects/effect-angular-query/src/lib/effect-angular-query.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-effect-angular-query',
+  imports: [],
+  template: `
+    <p>
+      effect-angular-query works!
+    </p>
+  `,
+  styles: ``,
+})
+export class EffectAngularQuery {
+
+}
---
+++ projects/effect-angular-query/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of effect-angular-query
+ */
+
+export * from './lib/effect-angular-query';

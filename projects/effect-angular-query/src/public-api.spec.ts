d3a87b896fbc8462a2442ce6ed04866877f97052
Lukas heddendorp <lu.heddendorp@gmail.com>
2026-01-30 15:55:15 +0100

    feat(angular-query): Update public API exports

--- projects/effect-angular-query/src/public-api.spec.ts
+++ projects/effect-angular-query/src/public-api.spec.ts
@@ -1,7 +1,16 @@
 import * as PublicApi from './public-api';
 
 describe('effect-angular-query public API', () => {
-  it('exports EffectAngularQuery', () => {
-    expect(PublicApi.EffectAngularQuery).toBeTruthy();
+  it('exports the RPC query client helpers', () => {
+    expect(PublicApi.EffectRpcQueryClient).toBeTruthy();
+    expect(PublicApi.EFFECT_RPC_QUERY_CLIENT_CONFIG).toBeTruthy();
+    expect(PublicApi.provideEffectRpcQueryClient).toBeTruthy();
+  });
+
+  it('exports query key and options utilities', () => {
+    expect(PublicApi.createRpcQueryKey).toBeTruthy();
+    expect(PublicApi.createRpcQueryOptions).toBeTruthy();
+    expect(PublicApi.createRpcPathKey).toBeTruthy();
+    expect(PublicApi.createRpcQueryFilter).toBeTruthy();
   });
 });
--- projects/effect-angular-query/src/public-api.ts
+++ projects/effect-angular-query/src/public-api.ts
@@ -2,4 +2,8 @@
  * Public API Surface of effect-angular-query
  */
 
-export * from './lib/effect-angular-query';
+export * from './lib/effect-rpc-query-client';
+export * from './lib/rpc-query-key';
+export * from './lib/rpc-query-options';
+export * from './lib/rpc-query-path';
+export * from './lib/rpc-query-types';
